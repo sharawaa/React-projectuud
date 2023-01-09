ae04b6ebcc22b67f1a8ee4462b5dc8d2ed1ceb61
Morning <morning@admins-iMac.local>
2023-01-09 12:26:12 +0800

    angias 09

--- E-COMMERS/src/components/Main.jsx
+++ E-COMMERS/src/components/Main.jsx
@@ -1,23 +1,25 @@
 import Navbar from "./main component/Navbar";
 import Products from "./main component/Products";
 import Slider from "./main component/Slider";
-import "../Style/main.css"
+import "../Style/main.css";
 import Image from "./main component/Image";
 import Bottom from "./m-bottom/Bottom";
 import Aside from "./m-bottom/Aside";
 
 export default function Main() {
   return (
-    <div class="Container">
+    <div class="Container container">
       <Slider />
       <Navbar />
       <section className="productC">
-      <Products />
+        <Products />
       </section>
-      <section style={{width:"100%", display:"flex", justifyContent:"center"}}>
+      <section
+        style={{ width: "100%", display: "flex", justifyContent: "center" }}
+      >
         <Image />
       </section>
-      <section style={{display: "flex"}}>
+      <section style={{ display: "flex" }}>
         <Bottom />
         <Aside />
       </section>
